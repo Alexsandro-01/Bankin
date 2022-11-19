3d5055478ec03e34b7f044b56c0b8150269f911c
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-11-19 19:06:18 -0300

    feat: :sparkles: Adição do método update aminterface de account

--- back-end/src/interfaces/IAccountModel.ts
+++ back-end/src/interfaces/IAccountModel.ts
@@ -1,7 +1,9 @@
 import Accounts from '../database/models/Accounts';
+import { IUpdateAccount } from './IAccount';
 
 interface IAccountModel {
   create(): Promise<Accounts>
+  update(_payload: IUpdateAccount): Promise<[number]>
 }
 
 export default IAccountModel;
