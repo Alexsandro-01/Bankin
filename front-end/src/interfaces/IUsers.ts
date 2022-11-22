f4bd44bef25bda4cf6246aefaf3cb795c42038ce
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-11-22 09:44:22 -0300

    feat: :label: interface para os dados do usuário e sua conta

--- front-end/src/interfaces/IUsers.ts
+++ front-end/src/interfaces/IUsers.ts
@@ -1,4 +1,13 @@
-export interface IUser {
+export interface IUserRequest {
   username: string,
   password: string
+}
+
+export interface IUser {
+  id: number,
+  username: string,
+  accountId: number,
+  Account: {
+    balance: string
+  }
 }
