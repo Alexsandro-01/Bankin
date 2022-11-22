bca018aa5fddc260c786ad3893dc6be8c2edb535
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-11-22 09:48:14 -0300

    feat: :sparkles: Funções para realização de consulta a API
    para buscar o histórico de transações do usuário, e para realizar um cash-out

--- front-end/src/services/fetch.ts
+++ front-end/src/services/fetch.ts
@@ -1,6 +1,7 @@
-import { IUser } from "../interfaces/IUsers";
+import { ITransactions } from "../interfaces/ITransactions";
+import { IUser, IUserRequest } from "../interfaces/IUsers";
 
-export async function createUser(data: IUser) {
+export async function createUser(data: IUserRequest) {
   const url = 'http://localhost:3001/users/create';
 
   const dataJson = JSON.stringify(data);
@@ -19,7 +20,7 @@ export async function createUser(data: IUser) {
   return user;
 };
 
-export async function login(data: IUser) {
+export async function login(data: IUserRequest) {
   const url = 'http://localhost:3001/users/login';
 
   const dataJson = JSON.stringify(data);
@@ -36,4 +37,62 @@ export async function login(data: IUser) {
   const response = await fetch(url, obj);
   const user = await response.json();
   return user;
+};
+
+export async function cashOut(data: ITransactions, token: string) {
+  const url = 'http://localhost:3001/transactions/cash-out';
+
+  const dataJson = JSON.stringify({
+    ...data,
+    value: Number(data.value)
+  });
+
+  const obj = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+    body: dataJson
+  }
+  
+  const response = await fetch(url, obj);
+
+  const transaction = await response.json();
+  return transaction;
+};
+
+export async function getBalance(token: string) {
+  const url = 'http://localhost:3001/users/get-user';
+
+  const obj = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization:`Bearer ${token}`,
+    },
+  }
+  
+  const response = await fetch(url, obj);
+  const user = await response.json();
+  return user as IUser;
+};
+
+export async function getHistoric(query: string, token: string) {
+  const url = `http://localhost:3001/transactions/?filter=${query}`;
+
+  const obj = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  
+  const response = await fetch(url, obj);
+  const historic = await response.json();
+  return historic;
 };
