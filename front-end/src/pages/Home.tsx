d8adc295f3fa1dbfd8bcf8474f0a1111e91bf433
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-11-22 09:40:31 -0300

    feat: :sparkles: implementação da página home do usuário
    e toda a sua regra de negócio para exibir os dados do usuário e permitir movimentar sua conta

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,7 +1,87 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Transaction from "../components/Transactions";
+import { getUserOnStorage } from '../services/sessionStorage';
+import { getBalance } from '../services/fetch';
+import { IUser } from '../interfaces/IUsers';
+import Historic from '../components/Historic';
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
+import { TbLogout } from 'react-icons/tb';
+import '../styles/home.css';
+
 function Home() {
+  const [user, setUser] = useState<IUser>();
+  const [toggle, setToggle] = useState(false);
+  const navigate = useNavigate();
+
+  async function getUserBalance(token: string) {
+    const response = await getBalance(token);
+
+    setUser(response);
+  }
+
+  function logout() {
+    sessionStorage.removeItem('token');
+
+    navigate('/');
+  }
+
+  useEffect(() => {
+    const token = getUserOnStorage();
+
+
+    if (!token) {
+      navigate('/login');
+    }
+
+    getUserBalance(token as string);
+  }, []);
+
   return (
-    <main>
-      <h1>Home</h1>
+    <main className='home'>
+      <header>
+        <div>
+          <p>
+            Olá, { user?.username }
+          </p>
+          <div>
+            <button
+              className='logout-btn'
+              onClick={ () => logout() }
+            >
+              <TbLogout />
+            </button>
+          </div>
+        </div>
+      </header>
+
+      <div className='home-container'>
+        <section className='actual-balance'>
+          <div>
+            <p>Saldo disponível</p>
+            <p className='balance-value'>R$ { user?.Account.balance.replace('.', ',') }</p>
+          </div>
+        </section>
+
+        <section className='transactions'>
+          <button
+            className={ toggle ? 'toggle toggle-on' : 'toggle toggle-off' }
+            onClick={() => setToggle(!toggle)}
+          >
+            Transferir { toggle ? <FaAngleUp /> : <FaAngleDown /> }
+          </button>
+
+          {
+            toggle && (
+              <Transaction getBalance={ getUserBalance } />
+            )
+          }
+        </section>
+
+        <section className='historic'>
+          <Historic user={ user } />
+        </section>
+      </div>
     </main>
   )
 }
